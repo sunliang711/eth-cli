c937d672d6619dbd5aeef777ef06f00d529df67e
sunliang <sunliang711@163.com>
2021-06-16 13:53:53 +0800

    command description;

--- cmd/newAccount.go
+++ cmd/newAccount.go
@@ -27,13 +27,8 @@ import (
 // newAccountCmd represents the newAccount command
 var newAccountCmd = &cobra.Command{
 	Use:   "new-account",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "create new account",
+	Long: `create new account`,
 	Run: newAccount,
 }
 
--- cmd/transfer.go
+++ cmd/transfer.go
@@ -30,13 +30,16 @@ import (
 // transferCmd represents the transfer command
 var transferCmd = &cobra.Command{
 	Use:   "transfer",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "transfer eth cmd",
+	Long: `transfer eth from sender to receiver:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+available flags(no flags will enter interactive mode):
+    --rpc rpc url
+    --from sender sk
+    --to receiver address
+    --value value of eth to transfer(uint: eth/1e18)
+    --price optional gas price
+    --nonce optional nonce`,
 	Run: transfer,
 }
 
